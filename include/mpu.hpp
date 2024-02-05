7171c0148c07f4c6bc55fc63ca114ee55ec9c68e
shvass <akshayb@gmx.com>
2024-02-05 19:34:48 +0530

    added mpu init and read/write

--- include/mpu.hpp
+++ include/mpu.hpp
@@ -19,10 +19,62 @@
 #ifndef MPU_HPP
 #define MPU_HPP
 
+#include <driver/gpio.h>
+// #include <driver/i2c_master.h>
+#include <driver/i2c.h>
+
+#include "registerMap.hpp"
+
+#define MPU_CLK_PIN_DEFAULT  GPIO_NUM_32   // default I2C clock pin 
+#define MPU_DIO_PIN_DEFAULT  GPIO_NUM_33   // default I2C data pin
+#define MPU_DEFAULT_PORT I2C_NUM_0         // default ESP I2C port pin
+#define MPU_DEF_CLK_FREQ (400 * 1000)      // I2C operation frequency
+#define MPU_OPE_WAIT_DELAY 100             // I2C operation delay
+
+/**
+ * @brief class to handle mpu9250 operations
+ * 
+ */
 class mpu {
+
+public:
+
+    /**
+     * @brief Construct a new mpu object
+     * 
+     * @param clkPin gpio pin to use as I2C clock pin
+     * @param dioPin gpio pin to use as I2C data  pin
+     */
+    mpu(gpio_num_t clkPin = MPU_CLK_PIN_DEFAULT, gpio_num_t dioPin = MPU_DIO_PIN_DEFAULT, bool lsbZero = true);
+
+
+    /**
+     * @brief burst read bytes from device 
+     * 
+     * @param address address of the first byte to read
+     * @param buffer address of the buffer to write the data
+     * @param size number of bytes to read, by default it reads one byte
+     */
+    void read(uint8_t address, uint8_t* buffer, size_t size = 1);
+
+
+    /**
+     * @brief burst write bytes from device 
+     * 
+     * @param address address of the first byte to write
+     * @param buffer address of the buffer to write data from
+     * @param size number of bytes to write, by default it writes one byte
+     */
+
+    void write(uint8_t address, uint8_t* buffer, size_t size = 1);
+
+
+
+
 
 private:
 
+    uint8_t address = MPU9250_ADDRESS;
 
 };  
 #endif //  MPU_HPP
--- mpu.cpp
+++ mpu.cpp
@@ -17,3 +17,42 @@
 
 
 #include "mpu.hpp"
+
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+
+
+bool isI2cInit = false;
+
+mpu::mpu( gpio_num_t clkPin, gpio_num_t dioPin, bool lsbZero){
+
+    if(!isI2cInit){
+
+        i2c_config_t conf = {
+            .mode = I2C_MODE_MASTER,
+            .sda_io_num = dioPin,        
+            .scl_io_num = clkPin,        
+            .sda_pullup_en = GPIO_PULLUP_ENABLE,
+            .scl_pullup_en = GPIO_PULLUP_ENABLE,
+            .master = {.clk_speed = MPU_DEF_CLK_FREQ }, 
+            .clk_flags = 0,                         
+        };
+
+        i2c_param_config(MPU_DEFAULT_PORT, &conf);
+        ESP_ERROR_CHECK(i2c_driver_install(MPU_DEFAULT_PORT, I2C_MODE_MASTER, 0, 0, 0));
+        isI2cInit = true;
+    }
+
+    address = (lsbZero) ? (MPU9250_ADDRESS) : (MPU9250_ADDRESS | 0x01);
+}
+
+void mpu::read(uint8_t address, uint8_t *buffer, size_t size){
+    i2c_master_write_read_device(MPU_DEFAULT_PORT, this->address, &address, 1, buffer, size, pdMS_TO_TICKS(MPU_OPE_WAIT_DELAY));
+}
+
+
+void mpu::write(uint8_t address, uint8_t *buffer, size_t size){
+    i2c_master_write_to_device(MPU_DEFAULT_PORT, this->address, buffer, size, pdMS_TO_TICKS(MPU_OPE_WAIT_DELAY));
+}
